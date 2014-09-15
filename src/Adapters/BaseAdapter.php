e4ffcb444c59a41b026a1e26f5bf80b199021fe8
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2014-09-15 10:39:50 +0300

    Minor improvements

--- src/Adapters/BaseAdapter.php
+++ src/Adapters/BaseAdapter.php
@@ -3,8 +3,29 @@ namespace UkraineComUa\Adapters;
 
 abstract class BaseAdapter {
 
+	protected $authLogin;
+
+	protected $authToken;
+
+	protected $format = 'json';
+
+	public function __construct($authLogin, $authToken, $format = 'json') {
+		$this->authLogin = $authLogin;
+		$this->authToken = $authToken;
+		$this->format = $format;
+	}
+
 	protected function formatContent(array $data) {
-		
+		$response = [
+			'auth_login' => $this->authLogin,
+			'auth_token' => $this->authToken,
+			'format' => $this->format
+		];
+
+		/* @TODO here needs to be proper setting of fields */
+		foreach ($data as $key => $value) {
+			$response[$key] => $value;
+		}
 	}
 
 }
--- src/Adapters/GuzzleAdapter.php
+++ src/Adapters/GuzzleAdapter.php
@@ -3,19 +3,7 @@ namespace UkraineComUa\Adapters;
 
 class GuzzleAdapter extends BaseAdapter implements AdapterInterface {
 
-	protected $authLogin;
-
-	protected $authToken;
-
-	protected $format = 'json';
-
-	public function __construct($authLogin, $authToken, $format = 'json') {
-		$this->authLogin = $authLogin;
-		$this->authToken = $authToken;
-		$this->format = $format;
-	}
-
-	public function post($url, array $headers = array(), $content = '')
+	public function post($url, array $headers = [], array $content = [])
 	{
 		$headers['content-type'] = 'application/json';
 		$request = $this->client->post($url, $headers, $content);
